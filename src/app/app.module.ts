7ddd476e76e82e6fc50418db978a0f33331b2416
Marina Crachi <antonio.claudio.ab@gmail.com>
2017-11-25 16:42:39 -0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MapaPage } from "../pages/mapa/mapa";
+import { LugarProvider } from '../providers/lugar/lugar';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MapaPage } from "../pages/mapa/mapa";
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    LugarProvider
   ]
 })
 export class AppModule { }
--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LugarProvider } from "../../providers/lugar/lugar";
 
 /**
  * Generated class for the MapaPage page.
@@ -14,13 +15,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mapa.html',
 })
 export class MapaPage {
-  mapaUrl = "http://lorempixel.com/731/449"
-  localSelecionado = { description: false }
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  destinoSelecionado: any = {
+    descricao: false,
+    id: "teste"
+  };
+  mapaUrl = "http://lorempixel.com/731/449";
+  localSelecionado = {
+    descricao: false,
+    id: "teste"
+  };
+  lugares = [];
+  trajetos = [];
+  querSelecionarLocal = true;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public lugarProvider: LugarProvider) {
+
   }
 
   ionViewDidLoad() {
+    this.lugares = this.lugarProvider.getLugares();
+    this.trajetos = this.lugarProvider.getTrajetos();
     console.log('ionViewDidLoad MapaPage');
   }
+  selecionaLocal(event, lugar) {
+    if (this.querSelecionarLocal || this.localSelecionado == lugar) {
+      this.localSelecionado = lugar;
+      this.destinoSelecionado = {};
+      this.querSelecionarLocal = false;
+    }
+    else {
+      this.destinoSelecionado = lugar;
+      this.querSelecionarLocal = true;
+    }
 
+  }
+  trajetoCss() {
+    return this.localSelecionado.id +"-"+ this.destinoSelecionado.id
+  }
 }
